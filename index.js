4b79932de1753dfb967faf3cb9dcc69394f36713
Uloma <47474481+yu2100@users.noreply.github.com>
2023-03-20 14:16:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 window.addEventListener('load', ()=>{
-    const form= document.querySelector("#task-form");
-    const input= document.querySelector("#task-input");
-    const list= document.querySelector("#tasks");
+    let form= document.querySelector("#todo-form");
+    let input= document.querySelector("#todo-input");
+    let list= document.querySelector("#tasks");
 
 
     form.addEventListener('submit', (e)=>{
         e.preventDefault();
 
-        const task = input.value;
+        let task = input.value;
         if (!task) {
             return;
         } 
 
-        const task_div = document.createElement("div");
+        let task_div = document.createElement("div");
         task_div.classList.add("task");
         list.appendChild(task_div);
 
 
-        const task_content_div = document.createElement("div");
+        let task_content_div = document.createElement("div");
         task_content_div.classList.add("content");
         task_div.appendChild(task_content_div);
    
 
-        const task_input= document.createElement("input");
+        let task_input= document.createElement("input");
         task_input.classList.add("text");
         task_input.type = "text";
         task_input.value= task;
@@ -30,25 +30,21 @@ window.addEventListener('load', ()=>{
         task_content_div.appendChild(task_input);
 
 
-        const task_actions_div= document.createElement("div");
+        let task_actions_div= document.createElement("div");
         task_actions_div.classList.add("actions");
         task_div.appendChild(task_actions_div);
 
 
-        const task_edit_botton= document.createElement("button");
+        let task_edit_botton= document.createElement("button");
         task_edit_botton.classList.add("Edit");
         task_edit_botton.innerHTML = "Edit";
 
-        const task_delete_button= document.createElement("button");
+        let task_delete_button= document.createElement("button");
         task_delete_button.classList.add("Delete");
         task_delete_button.innerHTML = "Delete";
 
-        const task_completed_button= document.createElement("button");
-        task_completed_button.classList.add("Completed");
-        task_completed_button.innerHTML = "Completed";
 
         task_actions_div.appendChild(task_edit_botton);
-        task_actions_div.appendChild(task_completed_button);
         task_actions_div.appendChild(task_delete_button);
         
 
@@ -67,19 +63,13 @@ window.addEventListener('load', ()=>{
         });
 
         task_delete_button.addEventListener('click', ()=>{
-            if (confirm("Are you sure you want to delete this task?")) {
+            if (confirm("Do you want to delete this task?")) {
                 list.removeChild(task_div);
                 
 
             }
         })
         
-        task_completed_button.addEventListener('click', ()=>{
-            
-                task_input.style.textDecoration="line-through";
-                task_input.setAttribute("readonly", "readonly");
-               
-        })
 
    
         input.value = "";
